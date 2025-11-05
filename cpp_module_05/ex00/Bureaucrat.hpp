a0b81ec86d194ee6e0fd7afc3ee6509841deea5c
Ali Emir Dinç <aldinc@k1m04s06.42istanbul.com.tr>
2025-11-05 15:32:35 +0300

    .hpp file is done for ex00

--- cpp_module_05/ex00/Bureaucrat.hpp
+++ cpp_module_05/ex00/Bureaucrat.hpp
@@ -6,32 +6,46 @@
 /*   By: aldinc <aldinc@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 12:53:06 by aldinc            #+#    #+#             */
-/*   Updated: 2025/11/04 12:53:07 by aldinc           ###   ########.fr       */
+/*   Updated: 2025/11/05 15:30:19 by aldinc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+# include <string>
 # include <iostream>
+# include <exception>
 
 class Bureaucrat {
+	private:
+		const std::string _name;
+		int _grade;
 	public:
-		const std::string name;
-		int grade;
-		
 		Bureaucrat();
-		Bureaucrat(const Bureaucrat& a);
+		Bureaucrat(const Bureaucrat& other);
 		Bureaucrat &operator=(const Bureaucrat& other);
-		Bureaucrat &operator<<(const Bureaucrat& other);
 		~Bureaucrat();
-		
-		void GradeTooHighException();
-		void GradeTooLowException();
 
+		Bureaucrat(const std::string name, int grade);
+		
+		std::string getName() const;
 		int getGrade() const;
-		void getName() const;
+
+		void incrementGrade();
+		void decrementGrade();
+
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
 };
 
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
 
 #endif
