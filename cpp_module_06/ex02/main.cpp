a312d27a6e9a37bbb4e934e7b1f804e73cbe384a
Ali Emir Dinç <aldinc@k1m14s05.42istanbul.com.tr>
2026-01-19 20:06:44 +0300

    ex02 main done

---
+++ cpp_module_06/ex02/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldinc <aldinc@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 18:39:02 by aldinc            #+#    #+#             */
+/*   Updated: 2026/01/19 20:05:37 by aldinc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <exception>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *generate(void) {
+	int i = std::rand() % 3;
+	if (i == 0) return new A;
+	else if (i == 1) return new B;
+	return new C;
+}
+
+void identify(Base* p) {
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p) {
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		return ;
+	} catch (std::exception &e) {}
+
+	try {
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		return ;
+	} catch (std::exception &e) {}
+
+	try {
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		return ;
+	} catch (std::exception &e) {}
+}
+
+int main() {
+	std::srand(std::time(0));
+
+	Base* instance = generate();
+
+	std::cout << "Identify with pointer: ";
+	identify(instance);
+
+	std::cout << "Identify with reference: ";
+	identify(*instance);
+
+	delete (instance);
+	return 0;
+}
