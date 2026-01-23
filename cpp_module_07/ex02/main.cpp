4cf6d839023b610a842ca19eb0a18a366c7b420f
Ali Emir Dinç <aldinc@k1m15s03.42istanbul.com.tr>
2026-01-23 14:53:27 +0300

    ex02's main updated

--- cpp_module_07/ex02/main.cpp
+++ cpp_module_07/ex02/main.cpp
@@ -6,14 +6,11 @@
 /*   By: aldinc <aldinc@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 08:59:03 by aldinc            #+#    #+#             */
-/*   Updated: 2026/01/21 09:43:30 by aldinc           ###   ########.fr       */
+/*   Updated: 2026/01/23 14:52:21 by aldinc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
-
-#include "Array.hpp"
-#include <iostream>
 
 int main() {
 	try {
@@ -22,16 +19,38 @@ int main() {
 			numbers[i] = i * 2;
 
 		std::cout << "Numbers size: " << numbers.size() << std::endl;
-		
+
 		Array<int> copy = numbers;
 		copy[0] = 42;
-		
+
 		std::cout << "Original[0]: " << numbers[0] << std::endl;
 		std::cout << "Copy[0]: " << copy[0] << std::endl;
 
 		std::cout << "Testing exception..." << std::endl;
 		std::cout << numbers[10] << std::endl;
 
+	} catch (const std::exception& e) {
+		std::cerr << "Caught: " << e.what() << std::endl;
+	}
+
+	try {
+		Array<float> fl_nums(3);
+		for (unsigned int i = 0; i < fl_nums.size(); i++)
+			fl_nums[i] = i * 2.2;
+		std::cout << "Numbers size: " << fl_nums.size() << std::endl;
+
+		Array<float> copy2 = fl_nums;
+		copy2[0] = 42;
+
+		std::cout << "Original[0]: " << fl_nums[0] << std::endl;
+		std::cout << "Original[0]: " << fl_nums[1] << std::endl;
+		std::cout << "Original[0]: " << fl_nums[2] << std::endl;
+		std::cout << "Copy2[0]: " << copy2[0] << std::endl;
+
+		std::cout << "Testing exception..." << std::endl;
+		std::cout << fl_nums[10] << std::endl;
+		
+		
 	} catch (const std::exception& e) {
 		std::cerr << "Caught: " << e.what() << std::endl;
 	}
