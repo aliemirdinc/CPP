dde646be149b938de65c4b95c8e0f16a38223f4a
Ali Emir Dinç <aldinc@k1m03s08.42istanbul.com.tr>
2026-01-28 19:44:23 +0300

    ex02 is done

--- cpp_module_08/ex01/Span.hpp
+++ cpp_module_08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: aldinc <aldinc@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 13:13:18 by aldinc            #+#    #+#             */
-/*   Updated: 2026/01/27 13:25:33 by aldinc           ###   ########.fr       */
+/*   Updated: 2026/01/28 18:49:21 by aldinc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,11 @@ class Span {
 
 		void addNumber(int number);
 		template <typename T>
-        void addNumber(T begin, T end) {
-            if (_numbers.size() + std::distance(begin, end) > _n)
-                throw std::runtime_error("Span limit exceeded!");
-            _numbers.insert(_numbers.end(), begin, end);
-        }
+		void addNumber(T begin, T end) {
+			if (_numbers.size() + std::distance(begin, end) > _n)
+				throw std::runtime_error("Span limit exceeded!");
+			_numbers.insert(_numbers.end(), begin, end);
+		}
 		int shortestSpan();
 		int longestSpan();
 
